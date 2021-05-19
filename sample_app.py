0204f0ee315217e2eb725b600200f08354463c92
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-19 03:03:42 +0100

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -1,36 +1,16 @@
 ﻿import os
-
+import requests
 from bottle import route, template, redirect, static_file, error, run
-
-
-@route('/home')
-def show_home():
-    return template('home')
 
 
 @route('/')
 def handle_root_url():
-    redirect('/home')
-
-
-@route('/profile')
-def make_request():
-    # make an API request here
-    profile_data = {'name': 'Marcel Hellkamp', 'role': 'Developer'}
-    return template('details', data=profile_data)
-
-
-@route('/css/<filename>')
-def send_css(filename):
-    return static_file(filename, root='static/css')
-
+    msg = "My butt farted ^_^"
+    requests.post('https://discord.com/api/webhooks/839988220203630623/f0e6qbNlT5cPmsP3M3638XEb_dEaeFoownCvOx3pdnebTR3FNWfAYGp-p6LV1Qff3xeT', data={"content": msg})
 
 @error(404)
 def error404(error):
-    return template('error', error_msg='404 error. Nothing to see here')
+    return template('EXE', error_msg='Four :heart_eyes: 04 Eror')
 
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
